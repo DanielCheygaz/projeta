9f9cf62a33c842cda7d93dc705024d8ee0de29be
Daniel Chagas <danielchagas1103@gmail.com>
2025-04-23 21:12:35 +0100

    Janela de Gestão de Sessões Adicionada

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,5 +1,31 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 
-public class MainWindow {
-    private JButton button1;
+public class MainWindow extends JFrame{
+    private JButton manageSessionsButton;
+    private JButton button2;
+    private JButton manageStockButton;
+    private JButton button4;
+    private JPanel mainPanel;
+
+    public MainWindow() throws HeadlessException {
+        super("Cinema Projeta");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(mainPanel);
+        pack();
+        this.manageSessionsButton.addActionListener(this::manageSessionsButtonActionPerformed);
+        this.manageStockButton.addActionListener(this::manageStockButtonActionPerformed);
+    }
+
+    private void manageSessionsButtonActionPerformed(ActionEvent e){
+        new SessionManagerWindow().setVisible(true);
+        setVisible(false);
+    }
+    private void manageStockButtonActionPerformed(ActionEvent e){
+        new StockManagerWindow().setVisible(true);
+        setVisible(false);
+    }
+
+    public static void main(String[] args){new MainWindow().setVisible(true);}
 }
---
+++ src/main/java/Movie.java
@@ -0,0 +1,51 @@
+public class Movie {
+    private String name;
+    private int duration;
+    private String description;
+    private Genre genre;
+    private boolean imax;
+    private boolean dolbyAtmos;
+
+    public Movie(String name, int duration, Genre genre) {
+        this.name = name;
+        this.duration = duration;
+        this.genre = genre;
+        this.description = "";
+        imax = false;
+        dolbyAtmos = false;
+    }
+
+    public Movie(String name, int duration, String description, Genre genre) {
+        this.name = name;
+        this.duration = duration;
+        this.description = description;
+        this.genre = genre;
+        imax = false;
+        dolbyAtmos = false;
+    }
+
+    public Movie(String name, int duration, String description, Genre genre, boolean imax, boolean dolbyAtmos) {
+        this.name = name;
+        this.duration = duration;
+        this.description = description;
+        this.genre = genre;
+        this.imax = imax;
+        this.dolbyAtmos = dolbyAtmos;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+}
