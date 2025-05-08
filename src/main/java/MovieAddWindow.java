4131cf1df0d347af5e9d2f2b0306c864e79fdbf5
Daniel Chagas <danielchagas1103@gmail.com>
2025-05-08 18:29:57 +0100

    Implementação gráfica da janela add movies

--- src/main/java/MovieAddWindow.java
+++ src/main/java/MovieAddWindow.java
@@ -3,16 +3,21 @@ import java.awt.*;
 
 public class MovieAddWindow extends JFrame{
     private JTextField textFieldName;
-    private JSpinner unitsSpinner;
-    private JTextField textFieldPrice;
     private JButton cancelButton;
     private JButton saveButton;
     private JPanel mainPanel;
+    private JTextField textFieldDuration;
+    private JTextArea textAreaDescription;
+    private JRadioButton imaxRadioButton;
+    private JRadioButton dolbyAtmosRadioButton;
+    private JComboBox comboBox1;
 
     public MovieAddWindow() throws HeadlessException {
         super("Adicionar Filme");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setContentPane(mainPanel);
         pack();
+
+
     }
 }
