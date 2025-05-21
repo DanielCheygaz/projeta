e3908c43ca5ea31b9c57d161c47c2759fce049d0
Daniel Chagas <danielchagas1103@gmail.com>
2025-05-21 17:52:11 +0100

    GenreManagerWindow added

---
+++ src/main/java/GenreManagerWindow.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class GenreManagerWindow extends JFrame{
+    private JButton backButton;
+    private JScrollPane scrollPane;
+    private JTable genresTable;
+    private JButton addGenreButton;
+    private JButton removeGenreButton;
+    private JButton editGenreButton;
+    private JPanel mainPanel;
+    private DefaultTableModel tableModel;
+
+    public GenreManagerWindow() throws HeadlessException {
+        super("Gestão de Géneros");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(mainPanel);
+        pack();
+
+        scrollPane.getViewport().setBackground(Color.decode("2894892"));
+        String[] columns = {"Nome"};
+
+        tableModel = new DefaultTableModel(columns,0);
+
+        for(Genre genre: AppData.getInstance().getGenreList()){
+            Object[] row = {
+                    genre.getName()
+            };
+            tableModel.addRow(row);
+        }
+        genresTable.setModel(tableModel);
+        genresTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        this.backButton.addActionListener(this::backButtonPerformed);
+    }
+
+    private void backButtonPerformed(ActionEvent e){
+        new MovieManagerWindow().setVisible(true);
+        dispose();
+    }
+
+    public static void main(String[] args){
+        new GenreManagerWindow().setVisible(true);
+    }
+}
