2e9662110d61ddeba158c1ec7aebbf347f30248e
Daniel Chagas <danielchagas1103@gmail.com>
2025-05-29 19:33:55 +0100

    Movie Test Cases Implemented

--- src/test/java/MovieTestCase.java
+++ src/test/java/MovieTestCase.java
@@ -1,7 +1,71 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MovieTestCase {
+    private static Genre genre;
+    private static Movie movie;
 
-    
+    @BeforeEach
+    public void setUp(){
+        genre = new Genre("Ação");
+        movie =  new Movie("Avatar",180,"Saving pandora", genre,true,true);
+    }
+
+    @Test
+    public void testGetName(){
+        assertEquals("Avatar", movie.getName());
+    }
+
+    @Test
+    public void testGetDuration(){
+        assertEquals(180, movie.getDuration());
+    }
+
+    @Test
+    public void testGetGenre(){
+        assertEquals(genre,movie.getGenre());
+    }
+
+    @Test
+    public void testGetDescription(){
+        assertEquals("Saving pandora", movie.getDescription());
+    }
+
+    @Test
+    public void testIsImax(){
+        assertEquals(true, movie.isImax());
+    }
+
+    @Test
+    public void testIsDolbyAtmos(){
+        assertEquals(true, movie.isDolbyAtmos());
+    }
+
+    @Test
+    public void testUpdateMovie(){
+        Genre newGenre = new Genre("Suspance");
+        Movie newMovie = new Movie("Oppenheimer",173,"", newGenre,false,true);
+        movie.updateMovie("Oppenheimer",173,"", newGenre,false,true);
+        assertEquals(newMovie.toString(),movie.toString());
+    }
+
+    /*@Test
+    public void testAddGenres(){
+        AppData appData = new AppData.getInstance();
+        LinkedList<Genre> expectedGenreList = new LinkedList<>();
+
+        expectedGenreList.add(genre);
+
+        appData.addGenre(genre);
+
+        assertArrayEquals(expectedGenreList.toArray(), appData.getGenreList().toArray(),"A listas tão partidas!");
+    }*/
+
 }
