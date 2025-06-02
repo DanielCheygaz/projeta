948f9879c31cc1bb4f44d123f7baa6d3d2f357b9
Daniel Chagas <danielchagas1103@gmail.com>
2025-06-02 13:21:51 +0100

    Refactored Stock Test Case

--- src/test/java/StockTestCase.java
+++ src/test/java/StockTestCase.java
@@ -5,42 +5,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StockTestCase {
     private Product product;
-    private Stock stock;
     private int units;
 
     @BeforeEach
     public void setUp(){
-        product = new Product("Coca Cola", 1.2);
         units = (int)(Math.random()*101);
-        stock = new Stock(product, units);
+        product = new Product("Coca Cola", 1.2, units);
     }
 
     @Test
     public void testGetProductName(){
-        assertEquals("Coca Cola", stock.getProduct().getName());
+        assertEquals("Coca Cola", product.getName());
     }
 
     @Test
     public void testGetProductPrice(){
-        assertEquals(1.2,stock.getProduct().getPrice());
+        assertEquals(1.2,product.getPrice());
     }
 
     @Test
     public void testGetUnits(){
-        assertEquals(units, stock.getUnits());
+        assertEquals(units, product.getUnits());
     }
 
     @Test
     public void testAddUnits(){
-        stock.addUnits(10);
-        assertEquals(units+10, stock.getUnits());
+        product.addUnits(10);
+        assertEquals(units+10, product.getUnits());
     }
 
     @Test
     public void testEditStock(){
-        Stock newStock = new Stock(new Product("Pipocas", 1.5), 10);
-        stock.editStock("Pipocas",10,1.5);
-        assertEquals(newStock.toString(),stock.toString());
+        Product newProduct = new Product("Pipocas", 1.5, 10);
+        product.editProduct("Pipocas",1.5, 10);
+        assertEquals(newProduct.toString(),product.toString());
     }
 
 }
